af66e0240bea1aac1ade0a867235e2925e1faa56
Caesar <cezi8@vp.pl>
2018-03-04 17:32:00 +0100

    Change reset button logic and correct faulty question 5

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -14,7 +14,8 @@ import android.widget.Toast;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int correctAnswers = 0;
+    int correctAnswers;
+    int incorrectAnswers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         boolean q2answer2selected = q2answer2Checkbox.isChecked();
         CheckBox q2answer3Checkbox = findViewById(R.id.q2answer3_checkbox);
         boolean q2answer3selected = q2answer3Checkbox.isChecked();
+        CheckBox q2answer4Checkbox = findViewById(R.id.q2answer4_checkbox);
+        boolean q2answer4selected = q2answer4Checkbox.isChecked();
 
         RadioButton q3answer2Checkbox = findViewById(R.id.q3answer2_checkbox);
         boolean q3answer2selected = q3answer2Checkbox.isChecked();
@@ -63,23 +66,19 @@ public class MainActivity extends AppCompatActivity {
 
         if (q1answer2selected) {
             correctAnswers = correctAnswers + 1;
-        }
+        } else incorrectAnswers = incorrectAnswers + 1;
 
-        if (q2answer1selected) {
-            if (q2answer2selected) {
-                if (q2answer3selected) {
-                    correctAnswers = correctAnswers + 1;
-                }
-            }
-        }
+        if (q2answer1selected && q2answer2selected && q2answer3selected && !q2answer4selected) {
+            correctAnswers = correctAnswers + 1;
+        } else incorrectAnswers = incorrectAnswers + 1;
 
         if (q3answer2selected) {
             correctAnswers = correctAnswers + 1;
-        }
+        } else incorrectAnswers = incorrectAnswers + 1;
 
         if (q4answer3selected) {
             correctAnswers = correctAnswers + 1;
-        }
+        } else incorrectAnswers = incorrectAnswers + 1;
 
         EditText q5answerTextfield = findViewById(R.id.q5answer_textfield);
         String q5answer_answer = q5answerTextfield.getText().toString();
@@ -87,29 +86,29 @@ public class MainActivity extends AppCompatActivity {
             if (q5answer_answer.equals("T-Mobile G1")) {
                 correctAnswers = correctAnswers + 1;
             }
-        }
+        } else incorrectAnswers = incorrectAnswers + 1;
 
         if (q6answer1selected) {
             correctAnswers = correctAnswers + 1;
-        }
+        } else incorrectAnswers = incorrectAnswers + 1;
 
         if (q7answer1selected) {
             correctAnswers = correctAnswers + 1;
-        }
+        } else incorrectAnswers = incorrectAnswers + 1;
 
         if (q8answer2selected) {
             correctAnswers = correctAnswers + 1;
-        }
+        } else incorrectAnswers = incorrectAnswers + 1;
 
         if (q9answer4selected) {
             correctAnswers = correctAnswers + 1;
-        }
+        } else incorrectAnswers = incorrectAnswers + 1;
 
         if (q10answer1selected) {
             correctAnswers = correctAnswers + 1;
-        }
+        } else incorrectAnswers = incorrectAnswers + 1;
 
-        Toast.makeText(this, "You've finished the quiz! Correct answers: " + correctAnswers, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Finished!\nCorrect answers: " + correctAnswers + "\nIncorrect answers: " + incorrectAnswers, Toast.LENGTH_LONG).show();
 
         Button summaryButton;
         summaryButton = findViewById(R.id.summary_button);
@@ -122,8 +121,97 @@ public class MainActivity extends AppCompatActivity {
 
     public void resetScore(View v) {
         correctAnswers = 0;
+        incorrectAnswers = 0;
         Button summaryButton = findViewById(R.id.summary_button);
         summaryButton.setEnabled(true);
+
+        /*
+          Reset all checkboxes and Buttons states
+         */
+        RadioButton q1answer1Checkbox = findViewById(R.id.q1answer1_checkbox);
+        q1answer1Checkbox.setChecked(false);
+        RadioButton q1answer2Checkbox = findViewById(R.id.q1answer2_checkbox);
+        q1answer2Checkbox.setChecked(false);
+        RadioButton q1answer3Checkbox = findViewById(R.id.q1answer3_checkbox);
+        q1answer3Checkbox.setChecked(false);
+        RadioButton q1answer4Checkbox = findViewById(R.id.q1answer4_checkbox);
+        q1answer4Checkbox.setChecked(false);
+
+        CheckBox q2answer1Checkbox = findViewById(R.id.q2answer1_checkbox);
+        q2answer1Checkbox.setChecked(false);
+        CheckBox q2answer2Checkbox = findViewById(R.id.q2answer2_checkbox);
+        q2answer2Checkbox.setChecked(false);
+        CheckBox q2answer3Checkbox = findViewById(R.id.q2answer3_checkbox);
+        q2answer3Checkbox.setChecked(false);
+        CheckBox q2answer4Checkbox = findViewById(R.id.q2answer4_checkbox);
+        q2answer4Checkbox.setChecked(false);
+
+        RadioButton q3answer1Checkbox = findViewById(R.id.q3answer1_checkbox);
+        q3answer1Checkbox.setChecked(false);
+        RadioButton q3answer2Checkbox = findViewById(R.id.q3answer2_checkbox);
+        q3answer2Checkbox.setChecked(false);
+        RadioButton q3answer3Checkbox = findViewById(R.id.q3answer3_checkbox);
+        q3answer3Checkbox.setChecked(false);
+        RadioButton q3answer4Checkbox = findViewById(R.id.q3answer4_checkbox);
+        q3answer4Checkbox.setChecked(false);
+
+        RadioButton q4answer1Checkbox = findViewById(R.id.q4answer1_checkbox);
+        q4answer1Checkbox.setChecked(false);
+        RadioButton q4answer2Checkbox = findViewById(R.id.q4answer2_checkbox);
+        q4answer2Checkbox.setChecked(false);
+        RadioButton q4answer3Checkbox = findViewById(R.id.q4answer3_checkbox);
+        q4answer3Checkbox.setChecked(false);
+        RadioButton q4answer4Checkbox = findViewById(R.id.q4answer4_checkbox);
+        q4answer4Checkbox.setChecked(false);
+
+        EditText q5answerTextfield = findViewById(R.id.q5answer_textfield);
+        q5answerTextfield.setText("");
+
+        RadioButton q6answer1Checkbox = findViewById(R.id.q6answer1_checkbox);
+        q6answer1Checkbox.setChecked(false);
+        RadioButton q6answer2Checkbox = findViewById(R.id.q6answer2_checkbox);
+        q6answer2Checkbox.setChecked(false);
+        RadioButton q6answer3Checkbox = findViewById(R.id.q6answer3_checkbox);
+        q6answer3Checkbox.setChecked(false);
+        RadioButton q6answer4Checkbox = findViewById(R.id.q6answer4_checkbox);
+        q6answer4Checkbox.setChecked(false);
+
+        RadioButton q7answer1Checkbox = findViewById(R.id.q7answer1_checkbox);
+        q7answer1Checkbox.setChecked(false);
+        RadioButton q7answer2Checkbox = findViewById(R.id.q7answer2_checkbox);
+        q7answer2Checkbox.setChecked(false);
+        RadioButton q7answer3Checkbox = findViewById(R.id.q7answer3_checkbox);
+        q7answer3Checkbox.setChecked(false);
+        RadioButton q7answer4Checkbox = findViewById(R.id.q7answer4_checkbox);
+        q7answer4Checkbox.setChecked(false);
+
+        RadioButton q8answer1Checkbox = findViewById(R.id.q8answer1_checkbox);
+        q8answer1Checkbox.setChecked(false);
+        RadioButton q8answer2Checkbox = findViewById(R.id.q8answer2_checkbox);
+        q8answer2Checkbox.setChecked(false);
+        RadioButton q8answer3Checkbox = findViewById(R.id.q8answer3_checkbox);
+        q8answer3Checkbox.setChecked(false);
+        RadioButton q8answer4Checkbox = findViewById(R.id.q8answer4_checkbox);
+        q8answer4Checkbox.setChecked(false);
+
+        RadioButton q9answer1Checkbox = findViewById(R.id.q9answer1_checkbox);
+        q9answer1Checkbox.setChecked(false);
+        RadioButton q9answer2Checkbox = findViewById(R.id.q9answer2_checkbox);
+        q9answer2Checkbox.setChecked(false);
+        RadioButton q9answer3Checkbox = findViewById(R.id.q9answer3_checkbox);
+        q9answer3Checkbox.setChecked(false);
+        RadioButton q9answer4Checkbox = findViewById(R.id.q9answer4_checkbox);
+        q9answer4Checkbox.setChecked(false);
+
+        RadioButton q10answer1Checkbox = findViewById(R.id.q10answer1_checkbox);
+        q10answer1Checkbox.setChecked(false);
+        RadioButton q10answer2Checkbox = findViewById(R.id.q10answer2_checkbox);
+        q10answer2Checkbox.setChecked(false);
+        RadioButton q10answer3Checkbox = findViewById(R.id.q10answer3_checkbox);
+        q10answer3Checkbox.setChecked(false);
+        RadioButton q10answer4Checkbox = findViewById(R.id.q10answer4_checkbox);
+        q10answer4Checkbox.setChecked(false);
+
     }
 
 }
